ccc89b56070db0822906765f86cdc225358ba12e
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2023-10-28 13:45:26 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/serve/',
-    bare: 'https://YOURSIMPLEBARESERVER/edu/',
+    bare: 'https://YOURSIMPLEBARESERVER/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: "/uv/uv.handler.js",
