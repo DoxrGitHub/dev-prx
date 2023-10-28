6f3811f83c3392063a6d1c245fdf183dc2a7b90f
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2023-10-28 13:40:30 -0500

    Create uv.config.js
    CHANGE LINE 3 HERE

---
+++ uv/uv.config.js
@@ -0,0 +1,11 @@
+self.__uv$config = {
+    prefix: '/serve/',
+    bare: 'https://YOURSIMPLEBARESERVER/edu/',
+    encodeUrl: Ultraviolet.codec.plain.encode,
+    decodeUrl: Ultraviolet.codec.plain.decode,
+    handler: "/uv/uv.handler.js",
+    client: "/uv/uv.client.js",
+    bundle: "/uv/uv.bundle.js",
+    config: "/uv/uv.config.js",
+    sw: "/uv/uv.sw.js",
+};
