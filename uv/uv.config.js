30a04cdd6d111b1a6a79c46905bb14d1cf2ee409
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2023-11-04 14:57:24 -0500

    reverted to old UV, that fixed sh**

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,9 @@
 self.__uv$config = {
     prefix: '/serve/',
-    bare: 'https://YOURSIMPLEBARESERVER/edu/',
+    bare: 'https://YOURSIMPLEBARESERVER/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
     bundle: "/uv/uv.bundle.js",
     config: "/uv/uv.config.js",
     sw: "/uv/uv.sw.js",
